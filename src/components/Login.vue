bf61b8a1f16e24c2cf3dc45bb4d467fe9b17f84e
Vikrant Khajuria <vikrant.khajuria@nagarro.com>
2023-10-06 17:50:12 +0530

    vue project

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -26,7 +26,7 @@ export default {
     methods: {
         async login() {
 
-            let result = await axios.get(`//localhost:3000/users?email=${this.email}&password=${this.password}`);
+            let result = await axios.get(`http://localhost:3000/users?email=${this.email}&password=${this.password}`);
 
             console.warn(result)
             if (result.status == 200 && result.data.length > 0) {
