28e7b603257b951745b7cff7bf86dac77afb9ee8
Vikrant Khajuria <vikrant.khajuria@nagarro.com>
2023-10-06 17:34:07 +0530

    vue project

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: true,
-  publicPath: process.env.NODE_ENV === "production" ? "/onboard/" : "/",
+  publicPath: '/onboard/'
 })
